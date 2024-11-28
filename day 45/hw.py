93be79fd304adfd1240baa7e756708e7694df084
ganzoDevil <gligva2012@gmail.com>
2024-11-28 17:44:11 +0400

    second commit

---
+++ day 45/hw.py
@@ -0,0 +1,21 @@
+print("hello world")
+
+
+num=15
+num1=12
+print(num + num1)
+
+word="sa"
+word1="ba"
+print(word+word1)
+
+s=40
+d=20
+print(s/d)
+#იმიტომ გამოიტანა ფლოათი რომ დახრილი ხაზი აჩვენებს რამდენჯერ მოთავსთებდა
+
+print(1<2)
+print(1>2)
+print(1!=2)
+
+Print(1=2)
