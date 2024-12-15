266ebb7f768b84faed0599e921f5e7a90dedb0fe
ganzoDevil <gligva2012@gmail.com>
2024-12-15 14:11:21 +0400

    second commit

---
+++ day_41/cw.py
@@ -0,0 +1,6 @@
+list=["matematika","pizika","sporti","musika","biologia"]
+print("matematika (0)","pizika (1)","sporti (2)","musika (3)","biologia (4)")
+user_chois=int(input("chose index 0-4:"))
+print(list[user_chois])
+
+
---
+++ day_41/hw.py
@@ -0,0 +1,4 @@
+list=["chips","cake","burger","fries","naggets","Hot wings","cola"]
+print("chips","cake","burger","fries","naggets","Hot wings","cola")
+user_chois=int(input("chose index 0-6:"))
+print(list[user_chois])
